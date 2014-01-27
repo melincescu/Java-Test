d723ad2bbb29fb9f04c2e74541031c8359009a13
Gheorghe <melincescu@gmail.com>
2014-01-27 17:57:03 +0200

    mesaj

--- Play Git/src/edu/test/commm/T1.java
+++ Play Git/src/edu/test/commm/T1.java
@@ -4,4 +4,36 @@ package edu.test.commm;
  * Created by gheorghe on 1/27/14.
  */
 public class T1 {
+
+    int x,y,z;
+
+    public T1(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
 }
