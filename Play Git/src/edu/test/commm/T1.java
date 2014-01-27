62c94b9952c31089a127b7b8ca2785379908b5eb
Gheorghe <melincescu@gmail.com>
2014-01-27 18:01:16 +0200

    fasfasf

--- Play Git/src/edu/test/commm/T1.java
+++ Play Git/src/edu/test/commm/T1.java
@@ -5,7 +5,46 @@ package edu.test.commm;
  */
 public class T1 {
 
-    int x,y,z;
+    int x;
+    int y;
+    int z;
+    int g;
+    int h;
+    int i;
+
+    public int getJ() {
+        return j;
+    }
+
+    public void setJ(int j) {
+        this.j = j;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
+    public int getG() {
+        return g;
+    }
+
+    public void setG(int g) {
+        this.g = g;
+    }
+
+    int j;
 
     public T1(int x, int y, int z) {
         this.x = x;
